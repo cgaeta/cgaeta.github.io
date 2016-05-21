124acdf802daa3001d6ca8702ea99924f485ea15
cgaeta <datopher64@gmail.com>
2016-05-21 18:29:06 -0400

    testing php

--- txtRsp.php
+++ txtRsp.php
@@ -5,6 +5,10 @@
  * http://www.twilio.com/docs/libraries
 */
  
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require('Services/Twilio.php');
  
 /* Controller: Match the keyword with the customized SMS reply. */
